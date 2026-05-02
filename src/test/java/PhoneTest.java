bb673c5359ab30b3866772e47c23c1dae917fcc9
Viktoriia Konoplia <konoplya@ms.sumdu.edu.ua>
2026-05-02 13:23:01 +0200

    Update tests for inheritance version

--- src/test/java/PhoneTest.java
+++ src/test/java/PhoneTest.java
@@ -7,30 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 class PhoneTest {
 
-    /**
-     * Checks exception when invalid price is set.
-     */
     @Test
     void shouldThrowExceptionWhenInvalidValueInSetter() {
-        Manufacturer manufacturer = new Manufacturer("Apple", "USA");
         Phone phone = new Phone("Apple", "iPhone 15", 999.99, 2023,
-                128, PhoneType.SMARTPHONE, manufacturer);
+                128, PhoneType.SMARTPHONE);
 
         assertThrows(IllegalArgumentException.class, () -> {
             phone.setPrice(-10);
         });
     }
 
-    /**
-     * Checks exception when constructor data is invalid.
-     */
     @Test
     void shouldThrowExceptionWhenInvalidConstructorData() {
-        Manufacturer manufacturer = new Manufacturer("Samsung", "South Korea");
-
         assertThrows(IllegalArgumentException.class, () -> {
             new Phone("", "Galaxy", -5, 1999,
-                    -10, PhoneType.SMARTPHONE, manufacturer);
+                    -10, PhoneType.SMARTPHONE);
         });
     }
 }
